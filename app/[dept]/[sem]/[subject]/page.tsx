8a6a750171c6bb15d1b047b15a2151f4368544e6
Aswin S <aswinsanthoshachus@gmail.com>
2025-05-24 20:45:44 +0530

    Update page.tsx

--- app/[dept]/[sem]/[subject]/page.tsx
+++ app/[dept]/[sem]/[subject]/page.tsx
@@ -1,11 +1,9 @@
 import GoogleSheetData from "@/app/components/Sheet";
 
 export default async function List({ params }: { params: Promise<{ sem: number, dept: string, subject: string }> }) {
-    // Await the params object and destructure it
     const { sem, dept: branch, subject: sub } = await params;
   
-    // Create an array of strings representing the keywords
-    const keywords = [String(sem), branch, sub]; // Ensure everything is a string
+    const keywords = [String(sem), branch, sub];
   
     return (
         <div>
