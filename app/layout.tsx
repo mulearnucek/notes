502b410a33a94e62f3f186699768a838a4ac88eb
richardshaju <richardshaju66@gmail.com>
2025-06-18 22:13:24 +0530

    ADD: integrate Google Analytics and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "../components/header";
 import { DataProvider } from "@/lib/DataContext";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Notes UCEK",
@@ -17,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-no-repeat bg-cover bg-center bg-[url('https://random-image-pepebigotes.vercel.app/api/random-image')] backdrop-brightness-50 bg-black/50">
         <DataProvider>
-        <Header />
+          <Header />
           <div className="min-h-screen text-white flex flex-col justify-center items-center p-4 sm:p-6">
             <div className="absolute inset-0 -z-10">
               <div className="w-full h-full bg-[url('https://random-image-pepebigotes.vercel.app/api/random-image')] bg-no-repeat bg-cover bg-center sepia-50 brightness-75 "></div>
@@ -25,7 +26,7 @@ export default function RootLayout({
             {children}
           </div>
         </DataProvider>
-      
+        <GoogleAnalytics gaId="GTM-TNXJGR6N" />
       </body>
     </html>
   );
